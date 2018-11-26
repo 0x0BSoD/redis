f3863c0c3e0a3e8537f6e0f799cb1e597129b8b1
Alexander Simonov <zlodey23@gmail.com>
2018-11-26 12:10:20 +0300

    Del useless vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-set -euo pipefail
-
-: ${REDIS_PASS:=devpassword}
-: ${REDIS_PORT:=6397}
-: ${REDIS_BIND:=0.0.0.0}
-
 sed -i "s/requirepass {{REDIS_PASS}}/requirepass ${REDIS_PASS}/" /etc/redis/redis.conf
 sed -i "s/port {{REDIS_PORT}}/port ${REDIS_PORT}/" /etc/redis/redis.conf
 sed -i "s/bind {{REDIS_BIND}}/bind ${REDIS_BIND}/" /etc/redis/redis.conf
